84e291731e73f7c294b9e52f126e78b57c988c5d
dmygaien <dmygaenko@gmail.com>
2018-03-14 23:33:54 +0200

    Initial commit

---
+++ rabbitmq-producer/src/main/java/com/mygaienko/rabbitmqpractice/consumer/ProducerApplication.java
@@ -0,0 +1,12 @@
+package com.mygaienko.rabbitmqpractice.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProducerApplication.class, args);
+	}
+}
